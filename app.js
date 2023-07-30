eabb6a93c73d99ba01a622decf659eb1c559be46
piyush18 <piyushshrm211@gmail.com>
2023-07-30 12:34:57 +0530

    OK

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
-
 app.get('/products', (req, res) => {
   res.send([
     {
